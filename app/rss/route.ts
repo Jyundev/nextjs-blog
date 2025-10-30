d9b797dd6bf3dc2b892ce5771fdc3d3d07eb8963
장윤영 <jyy0803@dataeum.kr>
2025-10-30 10:07:29 +0900

    rss 수정

--- app/rss/route.ts
+++ app/rss/route.ts
@@ -6,58 +6,43 @@ export async function GET() {
   let allBlogs = await getBlogPosts();
   let allNotes = await getNotePosts();
 
+  const createItemXml = (post, type) => `
+  <item>
+    <title>${post.metadata.title}</title>
+    <link>${baseUrl}/blog/${post.slug}</link>
+    <pubDate>${new Date(post.metadata.publishedAt).toUTCString()}</pubDate>
+    <category>${type}</category>
+  </item>
+`;
+
   const blogItemsXml = allBlogs
-    .sort((a, b) => {
-      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
-        return -1;
-      }
-      return 1;
-    })
-    .map(
-      (post) =>
-        `<item>
-          <title>${post.metadata.title}</title>
-          <link>${baseUrl}/blog/${post.slug}</link>
-          <description>${post.metadata.summary || ""}</description>
-          <pubDate>${new Date(
-            post.metadata.publishedAt
-          ).toUTCString()}</pubDate>
-        </item>`
+    .sort(
+      (a, b) =>
+        new Date(b.metadata.publishedAt).getTime() -
+        new Date(a.metadata.publishedAt).getTime()
     )
+    .map((post) => createItemXml(post, "Blog"))
     .join("\n");
 
   const noteItemsXml = allNotes
-    .sort((a, b) => {
-      if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
-        return -1;
-      }
-      return 1;
-    })
-    .map(
-      (post) =>
-        `<item>
-          <title>${post.metadata.title}</title>
-          <link>${baseUrl}/blog/${post.slug}</link>
-          <description>${post.metadata.summary || ""}</description>
-          <pubDate>${new Date(
-            post.metadata.publishedAt
-          ).toUTCString()}</pubDate>
-        </item>`
+    .sort(
+      (a, b) =>
+        new Date(b.metadata.publishedAt).getTime() -
+        new Date(a.metadata.publishedAt).getTime()
     )
+    .map((post) => createItemXml(post, "Note"))
     .join("\n");
 
   const rssFeed = `<?xml version="1.0" encoding="UTF-8" ?>
-  <rss version="2.0">
-    <channel>
-        <title>CodeByYun</title>
-        <link>${baseUrl}</link>
-        <description>This is my blog RSS feed</description>
-        <description>BLOG</description>
-        ${blogItemsXml}
-        <description>NOTE</description>
-        ${noteItemsXml}
-    </channel>
-  </rss>`;
+<rss version="2.0">
+  <channel>
+    <title>CodeByYun</title>
+    <link>${baseUrl}</link>
+    <description>This is my blog RSS feed</description>
+    ${blogItemsXml}
+    ${noteItemsXml}
+  </channel>
+</rss>`;
 
   return new Response(rssFeed, {
     headers: {
